e728607f50ccfd7197bebf3ecb9595f8f257818a
yavuzc78 <yavuzc78@hotmail.com>
2018-11-16 02:03:20 +0300

    Update multihashing.cc

--- multihashing.cc
+++ multihashing.cc
@@ -600,25 +600,6 @@ NAN_METHOD(x16r) {
     x16r_hash(input, output, input_len);
 
     info.GetReturnValue().Set(Nan::NewBuffer(output, 32).ToLocalChecked());
-}
-NAN_METHOD(x16s) {
-
-    if (info.Length() < 1)
-        return THROW_ERROR_EXCEPTION("You must provide one argument.");
-
-    Local<Object> target = Nan::To<Object>(info[0]).ToLocalChecked();
-
-    if(!Buffer::HasInstance(target))
-        return THROW_ERROR_EXCEPTION("Argument should be a buffer object.");
-
-    char * input = Buffer::Data(target);
-    char *output = (char*) malloc(sizeof(char) * 32);
-
-
-    x16s_hash(input, output);
-
-    info.GetReturnValue().Set(Nan::NewBuffer(output, 32).ToLocalChecked());
-}
 
 
 NAN_METHOD(fresh) {
