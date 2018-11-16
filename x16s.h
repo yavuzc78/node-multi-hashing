e826b09e0b339b5f946927d924f541e2353a47c8
yavuzc78 <yavuzc78@hotmail.com>
2018-11-16 03:36:04 +0300

    Update x16s.h

--- x16s.h
+++ x16s.h
@@ -7,7 +7,7 @@ extern "C" {
 
 #include <stdint.h>
 
-void shield_x16s_hash(const char* input, char* output);
+void x16s_hash(const char* input, char* output, uint32_t len);
 
 #ifdef __cplusplus
 }
